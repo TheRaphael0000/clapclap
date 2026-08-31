add more genres and fix genre embedding computation

src/clapclap/clustering/clustering.py
@@ -39,6 +39,7 @@ class Clustering:
             self.embeddings = np.array(self.embeddings)
             self.songIds = np.array(self.songIds)
 
+        logger.info("Computing clusters")
         cluster_labels = self.method.get_cluster_labels(self.embeddings)
 
         if self.smart_naming:
@@ -70,7 +71,6 @@ class Clustering:
         te = TextFeatureExtractor()
         from torch.utils.data import DataLoader
 
-
         ds = GenreDataset()
         dl = DataLoader(ds, batch_size=128)
 
@@ -79,6 +79,8 @@ class Clustering:
         for i, b in enumerate(dl):
             logger.debug(f"Batch {i}")
             genres.extend(b)
+            # add quotes to ensure multi-word genre embeddings are correctly computed 
+            b = [f'"{bi}"' for bi in b]
             genres_embeddings.extend(te.clap(b))
 
         return np.array(genres), np.array(genres_embeddings)

src/clapclap/update/text_feature_extractor.py
@@ -3,7 +3,8 @@ import logging
 import torch
 import transformers
 from transformers import AutoTokenizer, ClapTextModelWithProjection
-from torch.utils.data import DataLoader, IterableDataset
+from torch.utils.data import DataLoader, IterableDataset, Dataset
+from clapclap.utils.genres import everynoise_genres, musicbrainz_genres
 
 from clapclap.utils import Timer
 from clapclap.utils.consts import CLAP_MODEL, CLAP_PROCESSOR
@@ -36,14 +37,15 @@ class TextFeatureExtractor:
                 embeddings.append(r)
         return embeddings
 
-class GenreDataset(IterableDataset):
+class GenreDataset(Dataset):
     def __init__(self):
-        pass
+        self.genres = list(set(everynoise_genres) | set(musicbrainz_genres))
 
-    def __iter__(self):
-        with open("src/clapclap/utils/musicbrainz_genres.txt", "r") as f:
-            for row in f:
-                yield row.replace("\n", "")
+    def __len__(self):
+        return len(self.genres)
+
+    def __getitem__(self, idx):
+        return f'{self.genres[idx]}'
 
 
 if __name__ == "__main__":