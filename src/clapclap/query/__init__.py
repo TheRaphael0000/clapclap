8dc90b69f74c85cb2cca7bad41301c3dabc24029
Raphael Margueron <raphael.margueron@gmail.com>
2026-09-25 16:39:35 +0200

    add interactive playlist creation

--- src/clapclap/query/__init__.py
+++ src/clapclap/query/__init__.py
@@ -15,7 +15,9 @@ def add_subparser(subparsers: _SubParsersAction[ArgumentParser]):
     query_subparser.add_argument("--json", "-j", action="store_true", help="Return a json formatted string")
     query_subparser.add_argument("--limit", "-l", type=int, default=20, help="Number of results")
     query_subparser.add_argument("--temperature", "-t", type=float, default=0, help="Standard deviation of the noise to add to the centroid [0,1]")
-    query_subparser.add_argument("--save", "-s", type=str, help="Save to navidrome playlist")
+    query_subparser.add_argument("--save", "-s", action="store_true", help="Save to navidrome playlist")
+    query_subparser.add_argument("--name", "-n", type=str, help="Name of the playlist")
+    query_subparser.add_argument("--interactive-save", "-i", action="store_true", help="Prompt for save after print")
 
     query_subparsers = query_subparser.add_subparsers(required=True)
 
@@ -26,8 +28,8 @@ def add_subparser(subparsers: _SubParsersAction[ArgumentParser]):
     )
     group = query_similarity_parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--path", "-p", type=str, help="The relative path to a file in the DB")
-    group.add_argument("--songId", "-e", type=str, help="Query by songId")
-    group.add_argument("--albumId", "-b", type=str, help="Query by albumId")
+    group.add_argument("--songId", "-s", type=str, help="Query by songId")
+    group.add_argument("--albumId", "-a", type=str, help="Query by albumId")
     group.add_argument("--artistId", "-r", type=str, help="Query by artistId")
     query_similarity_parser.add_argument("--json", "-j", action="store_true", help="Return a json formatted string")
     query_similarity_parser.set_defaults(func=similarity_command)
@@ -41,23 +43,31 @@ def add_subparser(subparsers: _SubParsersAction[ArgumentParser]):
     query_text_parser.set_defaults(func=query_text_command)
 
 
-def process_query(query, args):
+def process_query(query, args, name):
     if args.json:
         print(query.get_json())
     else:
         print(query.get_text())
-    if args.save:
-        query.save_to_playlist(args.save)
+    
+    save = args.save
+    name = name or "unnamed"
+
+    if args.interactive_save:
+        result = input(f"Save playlist '{name}'? (y/N): ")
+        save = result.lower() == "y"
+
+    if save:
+        query.save_to_playlist(name)
 
 def similarity_command(args):
     logger.debug("command query simlilarity")
     query = SimilarityQuery(limit=args.limit, temperature=args.temperature, path=args.path, songId=args.songId, albumId=args.albumId, artistId=args.artistId)
-    process_query(query, args)
+    process_query(query, args, args.name)
 
 
 def query_text_command(args):
     logger.debug("command query text")
     from .text_query import TextQuery
     query = TextQuery(limit=args.limit, temperature=args.temperature, text=args.text)
-    process_query(query, args)
+    process_query(query, args, args.name or args.text)
 
--- src/clapclap/query/query.py
+++ src/clapclap/query/query.py
@@ -54,7 +54,8 @@ class Query:
             else:
                 query_limit = self.limit
 
-            stmt = select(Embedding, proximity_expr).filter(proximity_expr > 0).order_by(self.order_by_factor * proximity_expr).limit(query_limit)
+            stmt = select(Embedding, proximity_expr).order_by(self.order_by_factor * proximity_expr).limit(query_limit)
+            # stmt = select(Embedding, proximity_expr).filter(proximity_expr > 0).order_by(self.order_by_factor * proximity_expr).limit(query_limit)
             results = session.execute(stmt).all()
 
             # softmax temperature
